a9ea72c647ea24c9ed0d1ea69ff46d0217f4be60
Bowen Xue <boxue@microsoft.com>
2018-01-10 13:56:31 -0800

    Modification in javascript-library recipe test.

--- cssUsage.src.js
+++ cssUsage.src.js
@@ -1782,6 +1782,29 @@ void function() { try {
 } catch (ex) { /* do something maybe */ throw ex; } }();
 
 /* 
+    RECIPE: JS LIBRARY
+    -------------------------------------------------------------
+    Author: Bowen Xue
+    Description: This recipe looks for JS libraries used in a list of MS websites.
+        ex, <script src="url">
+*/
+
+void function() {
+    window.CSSUsage.StyleWalker.recipesToRun.push( function JSLibrary( element, results) {
+        if(element.nodeName == 'SCRIPT') {
+            var srcValue = element.getAttribute('src');
+            if (srcValue !== undefined && srcValue != "")
+            {
+                results[srcValue] = results[srcValue] || { count: 0 };
+                results[srcValue].count++;
+            }
+        }
+        return results;
+    });
+}();
+
+
+/* 
     RECIPE: z-index on static flex items
     -------------------------------------------------------------
     Author: Francois Remy
