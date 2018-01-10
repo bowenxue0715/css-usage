2238791a8491e1568865abd6d1a9166e2dcefd20
Bowen Xue <boxue@microsoft.com>
2018-01-10 10:51:43 -0800

    Add new recipe to collect JS libraries used in a list of common MS websites.

---
+++ src/recipes/archive/javascript-library.js
@@ -0,0 +1,22 @@
+/* 
+    RECIPE: JS LIBRARY
+    -------------------------------------------------------------
+    Author: Bowen Xue
+    Description: This recipe looks for JS libraries used in a list of MS websites.
+        ex, <script src="url">
+*/
+
+void function() {
+    window.CSSUsage.StyleWalker.recipesToRun.push( function JSLibrary( element, results) {
+        if(element.nodeName == 'SCRIPT') {
+            var srcValue = element.getAttribute('src');
+            if (srcValue !== undefined && srcValue != "")
+            {
+                results[srcValue] = results[srcValue] || { count: 0 };
+                results[srcValue].count++;
+            }
+        }
+        return results;
+    });
+}();
+
